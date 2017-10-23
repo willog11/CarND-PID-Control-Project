72fa37b6e0f955d89e5cd04686b1f3acb43c44e1
wogrady <willog11@gmail.com>
2017-10-23 18:16:14 +0100

    Reducing max speed

--- src/main.cpp
+++ src/main.cpp
@@ -37,7 +37,7 @@ int main()
   pid_steer.Init(0.165, 0.001, 3.05, false);
   pid_speed.Init(0.15, 0.0, 0.02, false);
 
-  double target_speed = 45.0;
+  double target_speed = 40.0;
 
   h.onMessage([&pid_steer, &pid_speed, &target_speed](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
