607d28b2be4f91b3dfaa7a316c27a06ea5d727dc
wogrady <willog11@gmail.com>
2017-10-23 18:09:15 +0100

    Correcting build issues

--- src/main.cpp
+++ src/main.cpp
@@ -39,7 +39,7 @@ int main()
 
   double target_speed = 45.0;
 
-  h.onMessage([&pid_steer, &pid_speed, &target_speed, &steering_prev](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
+  h.onMessage([&pid_steer, &pid_speed, &target_speed](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
     // The 2 signifies a websocket event
